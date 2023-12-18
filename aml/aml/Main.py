076932b189b67b8401bcae10999d9de6998fae2f
JunnyJun <chbe5082@naver.com>
2023-12-18 23:47:16 +0900

    init

--- aml/aml/Main.py
+++ aml/aml/Main.py
@@ -1,4 +1,7 @@
+import VerificationModel as vm
+
 
 if __name__ == '__main__':
     # Create the main window
     print("Hello World!")
+    vm.VerificationModel(500,100,10)
--- aml/aml/VerificationModel.py
+++ aml/aml/VerificationModel.py
@@ -1,21 +1,45 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 
-class IdentityVerificationModel(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(IdentityVerificationModel, self).__init__()
-
-        # 완전 연결 레이어
+class AMLModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(AMLModel, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)
-        self.softmax = nn.Softmax(dim=1)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        # 순전파
-        x = x.view(x.size(0), -1)  # 평탄화(flatten)
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
-        x = self.softmax(x)
+        x = self.sigmoid(x)
         return x
+
+# 모델 초기화
+input_size = 10
+hidden_size = 128
+output_size = 1  # 이진 분류이므로 1
+model = AMLModel(input_size, hidden_size, output_size)
+
+# 손실 함수와 옵티마이저 정의
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# 학습 데이터 준비
+# TODO: 학습 데이터를 로드하거나 생성하는 부분을 추가해야 합니다.
+train_dataset = None
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+# 학습 루프
+num_epochs = 10
+for epoch in range(num_epochs):
+    for inputs, labels in train_dataloader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+print('Finished Training')
