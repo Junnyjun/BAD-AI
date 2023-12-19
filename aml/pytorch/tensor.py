4e793c292b9190da27f1290ccdbfc70df6e7692f
JunnyJun <chbe5082@naver.com>
2023-12-20 00:08:57 +0900

    dataloader

--- aml/pytorch/tensor.py
+++ aml/pytorch/tensor.py
@@ -1,5 +1,38 @@
 import torch
 import numpy as np
 
+# ARRAY
 data = [[1, 2],[3, 4]]
 x_data = torch.tensor(data)
+
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
+
+# ATTRIBUTE
+x_ones = torch.ones_like(x_data) # x_data의 속성을 유지합니다.
+print(f"Ones Tensor: \n {x_ones} \n")
+
+x_rand = torch.rand_like(x_data, dtype=torch.float) # x_data의 속성을 덮어씁니다.
+print(f"Random Tensor: \n {x_rand} \n")
+
+# RANDOM
+shape = (2,3,)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
+
+# OPERATIONS
+if torch.cuda.is_available():
+    tensor = torch.to('cuda')
+    tensor = torch.ones(4, 4)
+
+print(f"First row: {tensor[0]}")
+print(f"First column: {tensor[:, 0]}")
+print(f"Last column: {tensor[..., -1]}")
+tensor[:,1] = 0
+print(tensor)
+
